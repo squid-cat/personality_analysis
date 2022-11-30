430abc4e34b50e113cc7fec201be725b0a6d6a47
squid-cat <kaikai120626@gmail.com>
2022-11-30 20:40:54 +0900

    lintの導入、lintエラー部分を修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,12 @@
-import { FC, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { FC, useEffect } from "react";
+import { useRouter } from "next/router";
 
 const Index: FC = () => {
   const router = useRouter();
   useEffect(() => {
     router.push("/register");
-  }, []);
+  }, [router]);
   return <></>;
-}
+};
 
 export default Index;
