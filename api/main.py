a390108b96b7ef0bd4bae3318ad93994c332b7bb
squid-cat <kaikai120626@gmail.com>
2022-11-30 14:24:14 +0900

    flaskを導入し、test用のapiと動画をpostするAPIの作成

--- api/main.py
+++ api/main.py
@@ -1,23 +1,53 @@
-import sys
-import time
+import os
+import atexit
+
+# api周辺のライブラリ
+from flask import Flask, jsonify, request
 
 # 独自ライブラリのimport
 import docker_openface
 
-def main():
-  try:
-    # openfaceのインストラクタ作成
-    openface = docker_openface.Openface()
-    openface.start()
-    openface.copyToDocker("./api/data/input.mp4")
-    
-    # TEST: 5秒停止
-    time.sleep(5)
-  
-  finally:
-    # 処理終了時に必ず実行
-    openface.stop()
-    pass
+app = Flask(__name__)
+currentPath = os.getcwd()
 
-if __name__ == "__main__":
-    sys.exit(main())
+# openfaceのインストラクタ作成
+openface = docker_openface.Openface()
+openface.start()
+
+@app.route("/test", methods=["GET"])
+def getTest():
+  return jsonify({"message": "hello world!"}), 200
+
+@app.route("/movie", methods=["POST"])
+def postMovie():
+
+  # ファイルがなかった場合、2つ以上の場合
+  if len(request.files) == 0:
+    return jsonify({"message": "Could not found files."}), 400
+  elif len(request.files) > 1:
+    return jsonify({"message": "Only one file can be sent."}), 400
+
+  if 'file' not in request.files:
+    return jsonify({"message": "Could not found send name \"file\"."}), 400
+
+  # データの取り出し
+  upload_file = request.files['file']
+
+  # 拡張子が動画ファイルでない場合
+  if not upload_file.filename.endswith(".mp4"):
+    return jsonify({"message": "Not a video file."}), 400
+
+  # 動画をアップロード
+  upload_file.save(currentPath + "/api/data/input.mp4")
+  # dockerに転送
+  openface.copyToDocker(currentPath + "/api/data/input.mp4")
+  return jsonify({"message": "Success uploaded."}), 200
+
+
+# 終了時に必ず実行
+@atexit.register
+def exeStop():
+  openface.stop()
+
+
+app.run()
