349c86b42ee2717646ac32694cf12342c1a136da
squid-cat <kaikai120626@gmail.com>
2022-12-01 00:41:11 +0900

    API取得をまとめるファイルの生成

---
+++ src/service/API.ts
@@ -0,0 +1,39 @@
+import { IConnect } from "@/types";
+import axios from "axios";
+
+const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "*",
+    "Content-Type": "application/json",
+  },
+});
+
+// [GET] API接続テスト
+const getConnect = () => {
+  const res = axiosInstance
+    .get<IConnect>("/connect")
+    .then((res) => res.data)
+    .catch((e) => {
+      console.error(e);
+      return null;
+    });
+  return res;
+};
+
+const fetchConnect = () => {
+  const res = fetch(BASE_URL + "/connect", {
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+  }).then((r) => r.json());
+  return res;
+};
+
+export const API = {
+  getConnect,
+  fetchConnect,
+};
