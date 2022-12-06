1569dffec81074bd48b980940e51288169efee25
squid-cat <kaikai120626@gmail.com>
2022-12-07 04:02:32 +0900

    不要なimportの削除

--- pages/analysis/wait/index.tsx
+++ pages/analysis/wait/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { FC, useCallback, useEffect, useRef } from "react";
 import { useRouter } from "next/router";
 import { API } from "@/service/API";
 
